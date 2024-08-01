c7629cd8bdc77c2078ebd048dc83857e2b4ae542
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2024-08-01 09:46:35 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,61 @@
+vim.wo.number = true
+vim.opt.clipboard="unnamed,unnamedplus"
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local uv = vim.uv or vim.loop
+
+-- Auto-install lazy.nvim if not present
+if not uv.fs_stat(lazypath) then
+  print('Installing lazy.nvim....')
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+  print('Done.')
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  { "savq/melange-nvim" },
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
+  {'numToStr/Comment.nvim'},
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  }
+})
+
+-- colorscheme
+vim.opt.termguicolors = true
+vim.cmd.colorscheme 'melange'
+
+-- commentiong with 'gcc'
+require('Comment').setup()
+
+-- toggle term
+require("toggleterm").setup{
+  open_mapping = [[<c- >]],
+  direction ='float',
+}
+
+require('flash').setup()
