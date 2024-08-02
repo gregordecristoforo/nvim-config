bd2887537abb95a6a7c05599fc063e2033c657da
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2024-08-02 12:46:27 +0200

    add harpoon

--- init.lua
+++ init.lua
@@ -40,6 +40,7 @@ require('lazy').setup({
 -- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' }
   },
+  {'ThePrimeagen/harpoon'},
   {
     "folke/flash.nvim",
     event = "VeryLazy",
@@ -101,3 +102,47 @@ vim.keymap.set('n', ' ff', builtin.find_files, {})
 vim.keymap.set('n', ' fg', builtin.live_grep, {})
 vim.keymap.set('n', ' fb', builtin.buffers, {})
 vim.keymap.set('n', ' fh', builtin.help_tags, {})
+
+--harpoon
+require('harpoon').setup({
+	global_settings = {
+	    -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+	    save_on_toggle = false,
+
+	    -- saves the harpoon file upon every change. disabling is unrecommended.
+	    save_on_change = true,
+
+	    -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+	    enter_on_sendcmd = false,
+
+	    -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+	    tmux_autoclose_windows = false,
+
+	    -- filetypes that you want to prevent from adding to the harpoon list menu.
+	    excluded_filetypes = { "harpoon" },
+
+	    -- set marks specific to each git branch inside git repository
+	    mark_branch = false,
+
+	    -- enable tabline with harpoon marks
+	    tabline = false,
+	    tabline_prefix = "   ",
+	    tabline_suffix = "   ",
+	}
+})
+require('telescope').load_extension('harpoon')
+
+vim.keymap.set('n', ' ha', require('harpoon.mark').add_file)
+vim.keymap.set('n', ' hc', require('harpoon.mark').clear_all)
+vim.keymap.set('n', ' hn', require('harpoon.ui').nav_next)
+vim.keymap.set('n', ' hp', require('harpoon.ui').nav_prev)
+vim.keymap.set('n', ' hh', ':Telescope harpoon marks<CR>')
+
+local function remove_current_file()
+    local harpoon_mark = require('harpoon.mark')
+    local current_file = vim.fn.expand('%:p')  -- Get the full path of the current file
+
+    harpoon_mark.rm_file(current)  -- Remove the current file from Harpoon
+end
+
+vim.keymap.set('n', ' hr', remove_current_file)
